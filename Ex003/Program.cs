338cbc492ed5f1960f3aba715173ee579a0a540b
Alesya Boyarina <l_i_s_a11@mail.ru>
2022-08-31 23:28:33 +0300

    Add Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введи N");
+int N = int.Parse (Console.ReadLine());
+int i = 1;
+while (i<N+1) 
+{
+    double cube_i = Math.Pow(i,3);
+    Console.Write($"{cube_i}, ");
+    i++;
+}
