785ec6fa9e4155791da5fb36c0f245c8f9178f52
Alesya Boyarina <l_i_s_a11@mail.ru>
2022-08-31 23:28:21 +0300

    Add Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход координаты 
+//двух точек и находит расстояние между ними в 3D пространстве.
+Console.Clear();
+Console.WriteLine("Введи x1");
+int x1 = int.Parse (Console.ReadLine());
+Console.WriteLine("Введи y1");
+int y1 = int.Parse (Console.ReadLine());
+Console.WriteLine("Введи z1");
+int z1 = int.Parse (Console.ReadLine());
+Console.WriteLine("Введи x2");
+int x2 = int.Parse (Console.ReadLine());
+Console.WriteLine("Введи y2");
+int y2 = int.Parse (Console.ReadLine());
+Console.WriteLine("Введи z2");
+int z2 = int.Parse (Console.ReadLine());
+double S = (Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)));
+Console.WriteLine($"Расстояние между двумя точками равно {S}");
