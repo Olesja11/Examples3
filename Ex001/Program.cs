e7e713e2bb40d868ccfc54ffb6e9e14ad9cb24d0
Alesya Boyarina <l_i_s_a11@mail.ru>
2022-08-31 23:28:06 +0300

    Add Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, является ли оно палиндромом.
+Console.Clear();
+Console.WriteLine("Введи число");
+int chislo = int.Parse (Console.ReadLine());
+int numA = chislo/1000;
+int numB = chislo%10*10 + chislo%100/10;
+if (numA == numB) 
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
