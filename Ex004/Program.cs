5f2cf6535bd5586defbfdbb0bbdad085053b6b48
Alesya Boyarina <l_i_s_a11@mail.ru>
2022-09-01 18:23:10 +0300

    Add Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает радиус круга
+//и находит его площадь округленную до целого числа,
+// необходимо вывести максимальную цифру в полученном
+// округлённом значении площади круга
+
+Console.Clear();
+Console.WriteLine("Введи радиус круга");
+int radius = int.Parse (Console.ReadLine());
+double S = Math.Round(Math.Pow(radius,2) * Math.PI);
+double  max = 1;  
+ while (S>0)
+ {
+    double i = S % 10;
+    S = S / 10;
+    if (i > max)
+    {
+        max = i;
+        }
+
+ }
+Console.WriteLine(max);
